b6664026fd3dfbdd71a926a12c645e8e1e26c942
Maziar <m.gh.korzani@gmail.com>
2016-05-10 10:07:07 +1000

    Modified copyright

--- Current-flow.cpp
+++ Current-flow.cpp
@@ -1,76 +1,78 @@
-/************************************************************************
- * MechSys - Open Library for Mechanical Systems                        *
- * Copyright (C) 2013 Maziar Gholami Korzani and Sergio Galindo Torres  *
- *                                                                      *
- * This program is free software: you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation, either version 3 of the License, or    *
- * any later version.                                                   *
- *                                                                      *
- * This program is distributed in the hope that it will be useful,      *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         *
- * GNU General Public License for more details.                         *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with this program. If not, see <http://www.gnu.org/licenses/>  *
- ************************************************************************/
+/***********************************************************************************
+* PersianSPH - A C++ library to simulate Mechanical Systems (solids, fluids        * 
+*             and soils) using Smoothed Particle Hydrodynamics method              *   
+* Copyright (C) 2013 Maziar Gholami Korzani and Sergio Galindo-Torres              *
+*                                                                                  *
+* This file is part of PersianSPH                                                  *
+*                                                                                  *
+* This is free software; you can redistribute it and/or modify it under the        *
+* terms of the GNU General Public License as published by the Free Software        *
+* Foundation; either version 3 of the License, or (at your option) any later       *
+* version.                                                                         *
+*                                                                                  *
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY  *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  *
+* PARTICULAR PURPOSE. See the GNU General Public License for more details.         *
+*                                                                                  *
+* You should have received a copy of the GNU General Public License along with     *
+* PersianSPH; if not, see <http://www.gnu.org/licenses/>                           *
+************************************************************************************/
 
 #include "Domain.h"
 #include "Interaction.h"
 
-double H,U,RhoF,RhoS,g,D,CsW,CsS,Damp,DampTime;
+double H,U,RhoF,RhoS,g,D,CsW,CsS,Damp,DampTime,dx;
 int Check=0;
 
 void UserInFlowCon(Vec3_t & position, Vec3_t & Vel, double & Den, SPH::Boundary & bdry)
 {
-	if (position(1)<=D)
+	if (position(1)<=(D+dx))
 		Vel = 0.0 , 0.0 , 0.0;
-	if (position(1)<(0.514*H+D) && position(1)>D)
-		Vel = pow(((position(1)-D)/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
-	if (position(1)>=(0.514*H+D))
+	if (position(1)<(0.514*H+D+dx) && position(1)>(D+dx))
+		Vel = pow(((position(1)-(D+dx))/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
+	if (position(1)>=(0.514*H+D+dx))
 		Vel = 1.07*U , 0.0 , 0.0;
 
 	if (position(1)<=D)
-		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*H+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
+		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*(H+dx)+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
 	else
-		Den = RhoF*pow((1+7.0*g*(H+D-position(1))/(CsW*CsW)),(1.0/7.0));
+		Den = RhoF*pow((1+7.0*g*(H+D+dx-position(1))/(CsW*CsW)),(1.0/7.0));
 }
 
 void UserAllFlowCon(Vec3_t & position, Vec3_t & Vel, double & Den, SPH::Boundary & bdry)
 {
-	if (position(1)<=D)
+	if (position(1)<=(D+dx))
 		Vel = 0.0 , 0.0 , 0.0;
-	if (position(1)<(0.514*H+D) && position(1)>D)
-		Vel = pow(((position(1)-D)/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
-	if (position(1)>=(0.514*H+D))
+	if (position(1)<(0.514*H+D+dx) && position(1)>(D+dx))
+		Vel = pow(((position(1)-(D+dx))/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
+	if (position(1)>=(0.514*H+D+dx))
 		Vel = 1.07*U , 0.0 , 0.0;
 
 	if (position(1)<=D)
-		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*H+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
+		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*(H+dx)+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
 	else
-		Den = RhoF*pow((1+7.0*g*(H+D-position(1))/(CsW*CsW)),(1.0/7.0));
+		Den = RhoF*pow((1+7.0*g*(H+D+dx-position(1))/(CsW*CsW)),(1.0/7.0));
 }
 
 void UserOutFlowCon(Vec3_t & position, Vec3_t & Vel, double & Den, SPH::Boundary & bdry)
 {
-	if (position(1)<=(D+4*0.005))
+	if (position(1)<=(D+dx))
 		Vel = 0.0 , 0.0 , 0.0;
-	if (position(1)<(0.514*H+D) && position(1)>(D+4*0.005))
-		Vel = pow(((position(1)-D)/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
-	if (position(1)>=(0.514*H+D))
+	if (position(1)<(0.514*H+D+dx) && position(1)>(D+dx))
+		Vel = pow(((position(1)-(D+dx))/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
+	if (position(1)>=(0.514*H+D+dx))
 		Vel = 1.07*U , 0.0 , 0.0;
 
 	if (position(1)<=D)
-		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*H+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
+		Den = (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*(H+dx)+(RhoS-RhoF)*(D-position(1)))),(1.0/7.0));
 	else
-		Den = RhoF*pow((1+7.0*g*(H+D-position(1))/(CsW*CsW)),(1.0/7.0));
+		Den = RhoF*pow((1+7.0*g*(H+D+dx-position(1))/(CsW*CsW)),(1.0/7.0));
 }
 
 void UserDamping(SPH::Domain & domi)
 {
 	if (domi.Time<DampTime)
-    #pragma omp parallel for schedule (static) num_threads(domi.Nproc)
+	#pragma omp parallel for schedule (static) num_threads(domi.Nproc)
 	for (size_t i=0; i<domi.Particles.Size(); i++)
 	{
 		if (domi.Particles[i]->IsFree) domi.Particles[i]->a -= Damp * domi.Particles[i]->v;
@@ -79,25 +81,25 @@ void UserDamping(SPH::Domain & domi)
 
 	if (domi.Time>(1.1*DampTime) && Check==0)
 	{
-		domi.BC.inv			= U,0.0,0.0;
-		domi.BC.outv		= U,0.0,0.0;
+		domi.BC.inv	= U,0.0,0.0;
+		domi.BC.outv	= U,0.0,0.0;
 		#pragma omp parallel for schedule (static) num_threads(domi.Nproc)
 		for (size_t i=0; i<domi.Particles.Size(); i++)
 		{
 			if (domi.Particles[i]->IsFree)
 			{
 				domi.Particles[i]->FirstStep = true;
-				if (domi.Particles[i]->x(1)<=D)
+				if (domi.Particles[i]->x(1)<=(D+dx))
 				{
 					domi.Particles[i]->v = 0.0 , 0.0 , 0.0;
 					domi.Particles[i]->vb = 0.0 , 0.0 , 0.0;
 				}
-				if (domi.Particles[i]->x(1)<(0.514*H+D) && domi.Particles[i]->x(1)>D)
+				if (domi.Particles[i]->x(1)<(0.514*H+D+dx) && domi.Particles[i]->x(1)>(D+dx))
 				{
-					domi.Particles[i]->v = pow(((domi.Particles[i]->x(1)-D)/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
-					domi.Particles[i]->vb = pow(((domi.Particles[i]->x(1)-D)/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
+					domi.Particles[i]->v = pow(((domi.Particles[i]->x(1)-(D+dx))/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
+					domi.Particles[i]->vb = pow(((domi.Particles[i]->x(1)-(D+dx))/(0.32*H)),(1.0/7.0))*U , 0.0 , 0.0;
 				}
-				if (domi.Particles[i]->x(1)>=(0.514*H+D))
+				if (domi.Particles[i]->x(1)>=(0.514*H+D+dx))
 				{
 					domi.Particles[i]->v = 1.07*U , 0.0 , 0.0;
 					domi.Particles[i]->vb = 1.07*U , 0.0 , 0.0;
@@ -115,62 +117,61 @@ using std::endl;
 
 int main(int argc, char **argv) try
 {
-        SPH::Domain		dom;
+        SPH::Domain	dom;
 
         dom.Dimension	= 2;
-        dom.Nproc		= 12;
-    	dom.VisEq		= 0;
+        dom.Nproc	= 10;
+    	dom.VisEq	= 0;
     	dom.KernelType	= 4;
-    	dom.Scheme		= 0;
-    	dom.Gravity		= 0.0 , -9.81 , 0.0 ;
-//    	dom.XSPH		= 0.5;
+    	dom.Scheme	= 0;
+    	dom.Gravity	= 0.0 , -9.81 , 0.0 ;
+//    	dom.XSPH	= 0.5;
 
-    	double x,y,Muw,Mus,T0,dx,h,t,t1,t2,t3,m;
+    	double x,y,Muw,Mus,T0,h,t,t1,t2,t3,m;
     	dx	= 0.005;
     	h	= dx*1.1;
-		D	= 0.1;
-		g	= norm(dom.Gravity);
-		H	= 4.0*D;
-		U	= 0.4;
-		RhoF= 998.21;
-		CsW	= 30.0;
-		x	= 4.0*D;
-		y	= 1.5*D + 1.0*dx;
-		Muw = 1.002e-3;
-		Mus = 0.002;
-		CsS = 30.0;
-		T0	= 3.79;
-		m	= 300.0;
-		RhoS= 2650.0;
-
+	D	= 0.1;
+	g	= norm(dom.Gravity);
+	H	= 4.0*D;
+	U	= 0.4;
+	RhoF	= 998.21;
+	CsW	= 30.0;
+	x	= 4.0*D;
+	y	= 1.5*D + 1.0*dx;
+	Muw	= 1.002e-3;
+	Mus	= 0.002;
+	CsS	= 30.0;
+	T0	= 3.79;
+	m	= 300.0;
+	RhoS	= 2650.0;
         t1	= (0.25*h/(CsW));
         t2	= (0.25*h/(CsS));
         t3	= 0.125*h*h*(RhoS-RhoF)/(Mus+m*T0);
         t	= std::min(t1,t2);
         t	= std::min(t,t3);
+
         std::cout<<"t1 = "<<t1<<std::endl;
         std::cout<<"t2 = "<<t2<<std::endl;
         std::cout<<"t3 = "<<t3<<std::endl;
         std::cout<<"t  = "<<t<<std::endl;
 
-        dom.GeneralBefore	= & UserDamping;
-//        dom.BC.Periodic[0]  = true;
-    	Damp	 			= 0.05*CsW/h;
-    	DampTime			= 0.0;
+    	Damp	 		= 0.05*CsW/h;
+    	DampTime		= 0.0;
     	dom.InitialDist 	= dx;
 
-		dom.BC.InOutFlow	= 3;
-//		dom.BC.inv			= 0.000001,0.0,0.0;
-//		dom.BC.outv			= 0.000001,0.0,0.0;
-		dom.BC.inDensity	= RhoF;
-		dom.BC.outDensity	= RhoF;
-		dom.BC.MassConservation = true;
+	dom.BC.InOutFlow	= 3;
+//	dom.BC.inv		= 0.000001,0.0,0.0;
+//	dom.BC.out		= 0.000001,0.0,0.0;
+	dom.BC.inDensity	= RhoF;
+	dom.BC.outDensity	= RhoF;
+	dom.BC.MassConservation = true;
 
-		dom.InCon	= & UserInFlowCon;
-		dom.OutCon	= & UserOutFlowCon;
+	dom.InCon		= & UserInFlowCon;
+	dom.OutCon		= & UserOutFlowCon;
+        dom.GeneralBefore	= & UserDamping;
 
 
-    	dom.AddBoxLength(1 ,Vec3_t ( 0.0 , -4.0*dx , 0.0 ), 10.0*D + dx/10.0 , 5.0*D + 4.0*dx + dx/10.0 ,  0 , dx/2.0 ,RhoF, h, 1 , 0 , false, false );
+    	dom.AddBoxLength(1 ,Vec3_t ( 0.0 , -4.0*dx , 0.0 ), 10.0*D + dx/10.0 , 5.0*D + 5.0*dx + dx/10.0 ,  0 , dx/2.0 ,RhoF, h, 1 , 0 , false, false );
 
     	double yb,xb;
 
@@ -186,8 +187,8 @@ int main(int argc, char **argv) try
     		dom.Particles[a]->MuRef		= Muw;
     		dom.Particles[a]->Material	= 1;
 //    		dom.Particles[a]->Shepard	= true;
-    		dom.Particles[a]->Density	= RhoF*pow((1+7.0*g*(H+D-yb)/(CsW*CsW)),(1.0/7.0));
-    		dom.Particles[a]->Densityb	= RhoF*pow((1+7.0*g*(H+D-yb)/(CsW*CsW)),(1.0/7.0));
+    		dom.Particles[a]->Density	= RhoF*pow((1+7.0*g*(H+D+dx-yb)/(CsW*CsW)),(1.0/7.0));
+    		dom.Particles[a]->Densityb	= RhoF*pow((1+7.0*g*(H+D+dx-yb)/(CsW*CsW)),(1.0/7.0));
 
 
     		if (yb<0.0)
@@ -212,9 +213,9 @@ int main(int argc, char **argv) try
         		dom.Particles[a]->MuRef		= Mus;
         		dom.Particles[a]->m 		= m;
         		dom.Particles[a]->T0 		= T0;
-        		dom.Particles[a]->Density	= (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*H+(RhoS-RhoF)*(D-yb))),(1.0/7.0));
-        		dom.Particles[a]->Densityb	= (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*H+(RhoS-RhoF)*(D-yb))),(1.0/7.0));
-        		dom.Particles[a]->RefDensity= RhoS-RhoF;
+        		dom.Particles[a]->Density	= (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*(H+dx)+(RhoS-RhoF)*(D-yb))),(1.0/7.0));
+        		dom.Particles[a]->Densityb	= (RhoS-RhoF)*pow((1+7.0*g/(CsS*CsS*(RhoS-RhoF))*(RhoF*(H+dx)+(RhoS-RhoF)*(D-yb))),(1.0/7.0));
+        		dom.Particles[a]->RefDensity	= RhoS-RhoF;
         		dom.Particles[a]->Mass		= dom.Particles[a]->Mass/RhoF*dom.Particles[a]->RefDensity;
     		}
 
@@ -238,10 +239,10 @@ int main(int argc, char **argv) try
     			xb = x + R*cos(2*M_PI/no*i);
     			yb = y + R*sin(2*M_PI/no*i);
     			dom.AddSingleParticle(4,Vec3_t ( xb ,  yb , 0.0 ), mass , RhoF , h , true);
-        		dom.Particles[dom.Particles.Size()-1]->Cs		= CsW;
+        		dom.Particles[dom.Particles.Size()-1]->Cs	= CsW;
         		dom.Particles[dom.Particles.Size()-1]->Alpha	= 0.05;
         		dom.Particles[dom.Particles.Size()-1]->PresEq	= 1;
-        		dom.Particles[dom.Particles.Size()-1]->Mu		= 1.002e-3;
+        		dom.Particles[dom.Particles.Size()-1]->Mu	= 1.002e-3;
         		dom.Particles[dom.Particles.Size()-1]->MuRef	= 1.002e-3;
         		dom.Particles[dom.Particles.Size()-1]->Material	= 1;
         		dom.Particles[dom.Particles.Size()-1]->NoSlip	= true;
@@ -249,7 +250,7 @@ int main(int argc, char **argv) try
     	}
 
 
-    	dom.Solve(/*tf*/50000.0,/*dt*/t,/*dtOut*/0.1,"test06",15000);
+    	dom.Solve(/*tf*/50000.0,/*dt*/t,/*dtOut*/0.001,"test06",15000);
         return 0;
 }
 MECHSYS_CATCH
--- Source/Interaction.h
+++ Source/Interaction.h
@@ -1,20 +1,22 @@
-/************************************************************************
- * MechSys - Open Library for Mechanical Systems                        *
- * Copyright (C) 2013 Maziar Gholami Korzani and Sergio Galindo Torres  *
- *                                                                      *
- * This program is free software: you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by *
- * the Free Software Foundation, either version 3 of the License, or    *
- * any later version.                                                   *
- *                                                                      *
- * This program is distributed in the hope that it will be useful,      *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the         *
- * GNU General Public License for more details.                         *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with this program. If not, see <http://www.gnu.org/licenses/>  *
- ************************************************************************/
+/***********************************************************************************
+* PersianSPH - A C++ library to simulate Mechanical Systems (solids, fluids        * 
+*             and soils) using Smoothed Particle Hydrodynamics method              *   
+* Copyright (C) 2013 Maziar Gholami Korzani and Sergio Galindo-Torres              *
+*                                                                                  *
+* This file is part of PersianSPH                                                  *
+*                                                                                  *
+* This is free software; you can redistribute it and/or modify it under the        *
+* terms of the GNU General Public License as published by the Free Software        *
+* Foundation; either version 3 of the License, or (at your option) any later       *
+* version.                                                                         *
+*                                                                                  *
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY  *
+* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  *
+* PARTICULAR PURPOSE. See the GNU General Public License for more details.         *
+*                                                                                  *
+* You should have received a copy of the GNU General Public License along with     *
+* PersianSPH; if not, see <http://www.gnu.org/licenses/>                           *
+************************************************************************************/
 
 // Std Lib
 #include <stdio.h>			/// for NULL
