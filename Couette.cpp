21675c0f94582d8b9af8c554c8098bb128680885
Maziar <m.gh.korzani@gmail.com>
2017-02-17 16:21:24 +1000

    before reprogramming the no-slip BC

--- Couette.cpp
+++ Couette.cpp
@@ -25,33 +25,42 @@
 using std::cout;
 using std::endl;
 
+void NewUserOutput(SPH::Particle * Particles, double & Prop1, double & Prop2,  double & Prop3)
+{
+	Prop1 = Particles->ShearRate;
+}
+
+
 int main(int argc, char **argv) try
 {
         SPH::Domain		dom;
 
-        dom.Dimension	= 2;
+        dom.Dimension		= 2;
         dom.BC.Periodic[0]	= true;
         dom.Nproc		= 8;
     	dom.VisEq		= 3;
-    	dom.KernelType	= 4;
+    	dom.KernelType		= 4;
     	dom.Scheme		= 0;
 
         double yb,h,rho;
     	double dx;
 
-    	rho = 998.21;
-    	dx = 2.5e-5;
-    	h = dx*1.1;
+    	rho	= 998.21;
+    	dx	= 2.5e-5;
+    	h	= dx*1.1;
     	dom.InitialDist 	= dx;
 
         double maz;
-        maz=(0.2*h/(0.08));
+        maz	=(0.2*h/(0.08));
         std::cout<<maz<<std::endl;
 
     	dom.AddBoxLength(3 ,Vec3_t ( 0.0 , -0.0000875001 , 0.0 ), 0.0005 , 0.00118  ,  0 , dx/2.0 ,rho, h, 1 , 0 , false, false );
 
     	for (size_t a=0; a<dom.Particles.Size(); a++)
     	{
+		dom.Particles[a]->LES		= true; //Just used to activate ShearRate calculation
+		dom.Particles[a]->CSmag		= 0.0;  //To deactive LES for the above purpose (No LES used for this simulation)
+
     		dom.Particles[a]->Cs		= 0.08;
     		dom.Particles[a]->PresEq	= 0;
     		dom.Particles[a]->Mu		= 1.002e-3;
@@ -75,8 +84,12 @@ int main(int argc, char **argv) try
     		}
     	}
 
-    	//    	dom.WriteXDMF("maz");
-    	dom.Solve(/*tf*/50000.0,/*dt*/maz,/*dtOut*/(1000.0*maz),"test06",1500);
+	dom.OutputName[0]	= "ShearRate";
+        dom.UserOutput		= & NewUserOutput;
+
+
+//    	dom.WriteXDMF("maz");
+    	dom.Solve(/*tf*/50000.0,/*dt*/maz,/*dtOut*/(1000.0*maz),"test06",250);
         return 0;
 }
 MECHSYS_CATCH
